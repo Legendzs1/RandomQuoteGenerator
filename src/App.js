767ba7143142d160bb22782be72659d87ce9234c
Legendzs1 <c.wagner7443@gmail.com>
2019-12-29 18:00:36 -0600

    Random Quote Generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+          loading:false,
+          quote: "",
+          author:"",
+
+        }
+         this.changeQuote = this.changeQuote.bind(this);
+    }
+    changeQuote() {
+      this.fetchData()
+    }
+    fetchData() {
+      fetch(`https://type.fit/api/quotes`)
+        .then(response => response.json())
+        .then(data =>{
+          //console.log(data[1246].text)
+          let quoteNum = Math.floor(Math.random() * data.length) //quote number
+          this.setState({
+            quote: data[quoteNum].text,
+            loading: false,
+            author:data[quoteNum].author
+          })
+        })
+    }
+    componentDidMount() {
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+      document.body.appendChild(script);
+      this.setState({
+        loading:true
+      })
+      this.fetchData()
+    }
+
+    render() {
+
+      const quote = this.state.loading ? "loading..." : this.state.quote;
+      const author = this.state.loading ? "loading..." : this.state.author;
+      const parentContainerStyles = {
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          display: 'table',
+          textAlign: 'center',
+          justifyContent:"center",
+          alignItems:"center"
+          //border: '4 solid #fff'
+        };
+        const subContainerStyles = {
+          position: 'relative',
+          height: '100%',
+          width: '100%',
+          display: 'table-cell',
+          verticalAlign: 'middle'
+        };
+        return (
+            <Card style={parentContainerStyles} className={"text-center"}>
+              <Card.Body style={subContainerStyles}>
+              <div id="quote-box">
+                <h1 id="text">{quote}</h1>
+                <h5 id="author">{author}</h5>
+                <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${this.quote} ${this.author}`} target='_blank' title="Post this quote on twitter!">
+                  <button type="button"
+                className="btn btn-dark btn-lg" id="">
+                Tweet Me!
+                  </button >
+                </a>
+                <button type="button" onClick={this.changeQuote}
+              className="btn btn-dark btn-lg" id="new-quote">
+              Change Me!
+                </button>
+              </div>
+              </Card.Body>
+            </Card>
+        )
+    }
 }
 
-export default App;
+export default App
